6706f9c71329e2044f6a555004c924c172ad39ab
April <120995993+apriljeanpollock@users.noreply.github.com>
2023-06-21 03:08:26 -0700

    Create home.js

---
+++ home.js
@@ -0,0 +1,11 @@
+function Home(){
+  return (
+    <Card
+      txtcolor="black"
+      header="April's Bank"
+      title="Welcome to the bank"
+      text="Please use this site for all your banking needs. Security not guaranteed."
+      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+    />    
+  );  
+}
